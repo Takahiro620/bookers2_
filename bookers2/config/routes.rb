35d87a909b10717b1b37851a5bbcdbb00ebdcd26
EC2 Default User <ec2-user@ip-172-31-28-254.ap-northeast-1.compute.internal>
2023-06-06 03:14:27 +0000

    16章終わり

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
   devise_for :users
